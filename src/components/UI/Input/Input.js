756de9a6b0600846d2f7eca99ac35556b8491967
Jakub Szymański <jszymanski.dev@gmail.com>
2022-08-15 19:29:14 +0200

    Add Input without icon

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,9 +9,9 @@ const Input = ({ type = "text", value: defaultValue, ...restProps }) => {
 	const passwordVisiblityIcon = isPasswordVisible ? faEyeSlash : faEye;
 
 	let labelClassName = "absolute -translate-y-1/2 text-gray-700 transition-all ease-linear";
-	labelClassName += isLabelOnTop ? " top-0 left-2 px-2 text-sm bg-white" : " top-1/2 left-[4.25rem] text-lg";
+	labelClassName += isLabelOnTop ? " top-0 left-2 z-20 px-2 text-sm bg-white" : ` top-1/2 ${restProps.icon ? "left-[4.25rem]" : "left-4"} -z-10 text-lg`;
 
-	let inputClassName = "w-full p-4 pl-[4.25rem] text-lg border border-solid border-gray-200 rounded-md";
+	let inputClassName = `w-full p-4 ${restProps.icon && "pl-[4.25rem]"} text-lg border border-solid border-gray-200 rounded-md bg-transparent`;
 	inputClassName += type === "password" ? " pr-11" : "";
 
 	const passwordIconHandler = () => {
@@ -25,9 +25,9 @@ const Input = ({ type = "text", value: defaultValue, ...restProps }) => {
 	};
 
 	return (
-		<div className="relative w-full my-8 rounded-md">
+		<div className="relative z-10 w-full my-8 rounded-md bg-white">
 			{restProps.icon && (
-				<FontAwesomeIcon icon={restProps.icon} className="absolute top-1/2 left-4 -translate-y-1/2 w-9 text-3xl text-gray-300" />
+				<FontAwesomeIcon icon={restProps.icon} className="absolute top-1/2 left-4 -z-10 -translate-y-1/2 w-9 text-3xl text-gray-300" />
 			)}
 			{type === "password" && (
 				<FontAwesomeIcon
